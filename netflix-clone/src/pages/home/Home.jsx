65a53807bcd0d130095d43a237459dc0e534ac5f
Haymisey <haymanotseyoum3@gmail.com>
2024-09-06 18:13:52 +0300

    header and footer

---
+++ netflix-clone/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
